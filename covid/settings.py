02abfbabb5811980fad1425c9c419c76175e4950
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2021-03-07 21:01:17 +0600

    hiding the APIs

--- covid/settings.py
+++ covid/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5af&a5nc8zt11v+$_$o%9y)5plec(+*fxa^ya*2n$(%cy#sgfu'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
