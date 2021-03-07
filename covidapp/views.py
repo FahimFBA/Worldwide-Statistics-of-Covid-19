0f35e4a4b1623e7d9a222d3281e8c28e098fc7e6
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2021-03-07 21:00:39 +0600

    hiding the APIs

--- covidapp/views.py
+++ covidapp/views.py
@@ -5,8 +5,7 @@ import json
 url = "https://covid-193.p.rapidapi.com/statistics"
 
 headers = {
-    'x-rapidapi-key': "90f88c6b30msh6878bf751006f7ap146ef6jsnde0275613b83",
-    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+    
     }
 
 response = requests.request("GET", url, headers=headers).json()
